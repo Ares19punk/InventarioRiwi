c3904ec440ce832b796ba68b6b911bd82338070d
Eduardo Pérez <eduardoperezpl1901@gmail.com>
2026-03-12 09:48:34 -0500

    2. Input date (variables in Python) (Task#854)

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Aplicaicon de consolo para el manejo de productos de los inventarios Riwi
 
 ## Entrada de datos (variables en Python):
 Se realizo la captura de datos de las variables: nombre, precio y cantidad
+
+## Task 2: 
+Es esta tarea se implemento la captura de datos ingresados por el usuario en relación al nombre, precio y cantidad
+Los datos capturados son:
+ - Nombre del prodcuto (Tipo String)
+ - Precio unitario (Tipo Float)
+ - Cantidad del prodcuto (Tipo Int)
+ Se aplico los condicionales if para garantizar la validez de los datos
+ Se aplico aplico ciclos "while" que solicita nuevamente la informacion si el usuario digita un valor incorrecto
---
+++ src/inventario.py
@@ -0,0 +1,51 @@
+
+#TASK 2: Captura de datos en consola
+#Este bloque solicita la informacion basica del producto: nombre, precio unitario y cantidad
+
+print("-"*35)
+print("REGISTRO DE PRODUCTO: ")
+print("-"*35)
+# ---------------------------------------------------
+# Parte 1: Captura del nombre del producto
+# La función input() permite leer datos desde la consola
+# ---------------------------------------------------
+name_producto = input("Por favor digite el nombre del producto: ")
+print("")
+
+# ---------------------------------------------------
+# Parte 2: Validación del precio del producto
+# Se utiliza un ciclo while para asegurar que el usuario
+# ingrese un valor numérico mayor que cero.
+# ---------------------------------------------------
+while precio <= 0:
+    precio = float(input("Digite el valor unitario del prodcuto: "))    
+    if precio > 0:
+        print("")
+        print("Precio digidato correctamente. $",precio)
+        print("")
+    else:
+        print("")
+        print("Por favor vuelva a digitar el precio. Valor incorrecto")
+        print("")
+# ---------------------------------------------------
+# Parte 3: Validación de la cantidad del producto
+# El ciclo while garantiza que la cantidad ingresada
+# sea un número entero mayor que cero.
+# ---------------------------------------------------
+while cantidad <= 0:
+    cantidad = int(input("Por favor digite la cantidad del prodcuto: "))
+    if cantidad > 0:
+        print("")
+        print("Cantidad digidata correctamente. $",cantidad)
+        print("")
+    else:
+        print("")
+        print("Por favor vuelva a digitar la cantidad. Valor incorrecto")
+        print("")
+
+
+
+
+
+
+  
