26d5bbf7652ff3fa5d886d1c5bd1cd6290bc9bb7
Eduardo de Jesús Pérez López <eduardoperezpl1901@gmail.com>
2026-03-19 14:36:42 -0500

    Docs: 4. Calculate basic statistics (task#1012)

--- src/menu.py
+++ src/menu.py
@@ -4,7 +4,7 @@ def agregar_producto():
     print("|                                   |")
     nombre = input("| a. Digite el nombre de producto  \n| >  ")
     precio = float(input("| b. Digite el precio del producto\n| >  "))
-    cantidad = input("| c. Digite la cantidad del producto\n| >  ")
+    cantidad = float(input("| c. Digite la cantidad del producto\n| >  "))
     print("|                                    |")
     producto = {"nombre" : nombre, 
               "precio" : precio,
@@ -15,8 +15,23 @@ def mostrar_inventario(lista):
     print("|>--------2. Mostrar inventario----<|")
     print()
     for i, nombre in enumerate(lista):
-        print(f" {i+1:<3}. Producto : {nombre["nombre"]:<12} | Precio : {nombre["precio"]:<8} | Cantidad : {nombre["cantidad"]} ")
+        print(f" {i+1:<3}. Producto : {nombre["nombre"]:<12} | Precio : {nombre["precio"]:<8} | Cantidad : {nombre["cantidad"]:<5} | Total : 2{nombre["cantidad"]*nombre["precio"]}")
     print()
+
+def calcular_estadistica(lista):
+    suma = 0
+    print("|>------3. Calcular estadistica----<|")
+    num_producto = len(lista)
+    for i, precio in enumerate(lista):
+        suma = (precio["precio"]*precio["cantidad"]) + suma
+    print("|                                   |")
+    print("| a. Valor total del inventario:    |")
+    print(f"|   > $ {suma:<8}                    |")
+    print("| b. Cantidad total de productos:   |")
+    print(f"|   > {num_producto:<3}                           |")
+    print("-"*35)
+
+print("Cantidad de productos registrados")
 print("-"*37)
 print("|     Sistema de Inventarios Riwi   |")
 print("-"*37)
@@ -57,6 +72,7 @@ while option < 4:
                 mostrar_inventario(lista_inv)
                 pass
             case 3:
+                calcular_estadistica(lista_inv)
                 pass
             case 4:
                 pass
